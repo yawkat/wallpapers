9a987325e732685ebd48999c4ec44a548a43ce68
Jonas Konrad <me@yawk.at>
2014-09-12 19:32:23 +0200

    First commit

---
+++ download.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+
+import os
+from os import path
+import urlparse
+import urllib
+
+items = set()
+
+os.makedirs("images")
+
+with open("links", "r") as f:
+    for item in f:
+        item = item.strip()
+        if item == "" or item[0] == '#':
+            continue
+        name = path.basename(urlparse.urlsplit(item).path)
+        if name in items:
+            continue
+        items.add(name)
+        f = path.join("images", name)
+        if path.isfile(f):
+            continue
+        print "Downloading '%s'..." % item
+        urllib.urlretrieve(item, f)
+
+for f in os.listdir("images"):
+    if not path.basename(f) in items:
+        print "Deleting '%s'..." % path.basename(f)
+        os.unlink(path.join("images", f))
